54c2005ef7c8c8b757071234e59d32d1de5ca8ae
Can Kırşallıoba <cankirsallioba@gmail.com>
2020-01-23 08:44:58 +0300

    problem013 solved

--- problem013/problem013.cpp
+++ problem013/problem013.cpp
@@ -108,5 +108,26 @@ Work out the first ten digits of the sum of the following one-hundred 50-digit n
 using namespace std;
 
 int main() {
-    
+    ifstream fin("problem013.txt");
+	string num[100];
+	for (int n = 0; n < 100; n++)
+		getline(fin, num[n]);
+	string sum(15, '\0');
+	for (int n = 0; n < sum.size() - 2; n++) {
+		int tSum = 0;
+		for (int m = 0; m < 100; m++)
+			tSum += num[m][n]-'0';
+		sum[n] += tSum/100;
+		sum[n+1] += tSum/10%10;
+		sum[n+2] += tSum%10;
+	}
+	for (int n = sum.size() - 1; n > 0; n--) {
+		if (sum[n] >= 10) {
+			sum[n] -= 10;
+			sum[n-1]++;
+		}
+	}
+	for (int n = 0; n < 10; n++)
+		cout << int(sum[n]);
+	cout << "\n";
 }
