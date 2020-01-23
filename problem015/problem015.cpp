4f26ddf86c6e6756c7a513075bd82ddaf8e8b629
Can Kırşallıoba <cankirsallioba@gmail.com>
2020-01-23 10:29:20 +0300

    problem 015 solved

--- problem015/problem015.cpp
+++ problem015/problem015.cpp
@@ -10,5 +10,13 @@ How many such routes are there through a 20×20 grid?
 using namespace std;
 
 int main () {
-    
+    long long grid[21][21] = {0};
+	grid[0][0] = 1;
+	for (int n = 1; n < 21; n++) {
+		for (int r = 0; r <= n; r++) {
+			grid[r][n] = (r>0 ? grid[r-1][n] : 0) + grid[r][n-1];
+			grid[n][r] = grid[r][n];
+		}
+	}
+	std::cout << grid[20][20] << "\n";
 }
